1749c436752469385aa1fe67beafc6142974039b
Masataka Yakura <masataka.yakura@gmail.com>
2018-10-28 18:05:53 +0900

    feat(IconSvg): add a MVP of IconSvg custom element class
    it accepts (in fact requires) three attributes to render an icon:
    
    * icon-source: url to an icon (either a path to an SVG file or a pointer to the symbol)
    * icon-width: width of an icon (in pixels)
    * icon-height: height of an icon (in pixels)
    
    currently it doesn't reflect changes in attributes.

---
+++ src/IconSvg.js
@@ -0,0 +1,33 @@
+class IconSVG extends HTMLElement {
+  constructor() {
+    super();
+
+    this._source = this.getAttribute(`icon-source`);
+    this._width = parseInt(this.getAttribute(`icon-width`)) || 0;
+    this._height = parseInt(this.getAttribute(`icon-height`)) || 0;
+
+    this.attachShadow({ mode: `open` });
+    this._shadow = this.shadowRoot;
+    this._shadow.innerHTML = `
+      <style>
+        :host {
+          display: inline-flex;
+        }
+        svg { width: ${this._width}px; height: ${this._height}px; }
+      </style>
+      <svg>
+        <use xlink:href="${this._source}"/>
+      </svg>
+    `;
+  }
+
+  static get observedAttributes() {}
+
+  connectedCallback() {}
+
+  disconnectedCallback() {}
+
+  attributeChangedCallback(name, oldValue, newValue) {}
+}
+
+export default IconSVG;
