9b50ea5ac12d01dd65e98bebfd44c04f4b6a1b98
Masataka Yakura <masataka.yakura@gmail.com>
2018-10-28 20:59:50 +0900

    feat(IconSvg): introduce getter and setter for attributes
    just copied and pasted. needs type checking and conversion.

--- src/IconSvg.js
+++ src/IconSvg.js
@@ -9,6 +9,31 @@ class IconSVG extends HTMLElement {
     this.height = 0;
   }
 
+  get width() {
+    return this.getAttribute(`icon-width`);
+  }
+  set width(value) {
+    if (this.getAttribute(`icon-width`) !== value) {
+      this.setAttribute(`icon-width`, value);
+    }
+  }
+  get height() {
+    return this.getAttribute(`icon-height`);
+  }
+  set height(value) {
+    if (this.getAttribute(`icon-height`) !== value) {
+      this.setAttribute(`icon-height`, value);
+    }
+  }
+  get source() {
+    return this.getAttribute(`icon-source`);
+  }
+  set source(value) {
+    if (this.getAttribute(`icon-source`) !== value) {
+      this.setAttribute(`icon-source`, value);
+    }
+  }
+
   connectedCallback() {
     this.source = this.getAttribute(`icon-source`);
     this.width = parseInt(this.getAttribute(`icon-width`)) || 0;
